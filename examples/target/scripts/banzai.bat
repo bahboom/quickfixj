b606b01dc8ca60d8f0a7d23fc15f7387ad2081a1
Ming Liu <nongnongd@yahoo.com>
2013-11-07 14:50:37 -0500

    first commit

---
+++ examples/target/scripts/banzai.bat
@@ -0,0 +1,24 @@
+@echo off
+
+REM Scripts for the QFJ source distribution (Java 5+)
+
+if "%OS%"=="Windows_NT" @setlocal
+if "%OS%"=="WINNT" @setlocal
+
+rem %~dp0 is expanded pathname of the current script under NT
+set QFJ_BIN=%~dp0
+set DEFAULT_QFJ_HOME=%QFJ_BIN%..\\..\\..
+
+if "%QFJ_HOME%"=="" set QFJ_HOME=%DEFAULT_QFJ_HOME%
+set DEFAULT_QFJ_HOME=
+
+if not "%QFJ_HOME%"=="" goto qfjHomeOk
+echo QFJ_HOME must set manually for older versions of Windows. Please set QFJ_HOME.
+goto end
+:qfjHomeOk
+
+set CP=%QFJ_HOME%/core/src/main/lib/mina-core-1.1.7.jar;%QFJ_HOME%/core/src/main/lib/slf4j-api-1.6.3.jar;%QFJ_HOME%/core/src/main/lib/slf4j-jdk14-1.6.3.jar;%QFJ_HOME%/core/target/quickfixj-all-.jar;%QFJ_HOME%/core/target/quickfixj-core-.jar;%QFJ_HOME%/core/target/quickfixj-msg-fix40-.jar;%QFJ_HOME%/core/target/quickfixj-msg-fix41-.jar;%QFJ_HOME%/core/target/quickfixj-msg-fix42-.jar;%QFJ_HOME%/core/target/quickfixj-msg-fix43-.jar;%QFJ_HOME%/core/target/quickfixj-msg-fix44-.jar;%QFJ_HOME%/core/target/quickfixj-msg-fix50-.jar;%QFJ_HOME%/core/target/quickfixj-msg-fixt11-.jar;%QFJ_HOME%/examples/target/quickfixj-examples-.jar
+
+java -cp %CP% quickfix.examples.banzai.Banzai %1 %2 %3 %4 %5 %6 %7 %8 %9
+
+:end
---
+++ examples/target/scripts/executor.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# Scripts for the QFJ source distribution (Java 5+)
+
+scriptdir=`dirname $0`
+qfjhome=$scriptdir/../../..
+
+CP=$qfjhome/core/src/main/lib/mina-core-1.1.7.jar:$qfjhome/core/src/main/lib/slf4j-api-1.6.3.jar:$qfjhome/core/src/main/lib/slf4j-jdk14-1.6.3.jar:$qfjhome/core/target/quickfixj-all-.jar:$qfjhome/core/target/quickfixj-core-.jar:$qfjhome/core/target/quickfixj-msg-fix40-.jar:$qfjhome/core/target/quickfixj-msg-fix41-.jar:$qfjhome/core/target/quickfixj-msg-fix42-.jar:$qfjhome/core/target/quickfixj-msg-fix43-.jar:$qfjhome/core/target/quickfixj-msg-fix44-.jar:$qfjhome/core/target/quickfixj-msg-fix50-.jar:$qfjhome/core/target/quickfixj-msg-fixt11-.jar:$qfjhome/examples/target/quickfixj-examples-.jar
+
+java -classpath "$CP"  quickfix.examples.executor.Executor $*
